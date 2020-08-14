e1fe770fbb3cbdf10448eafbca6a34ad72865393
fakhrilak <fakhrilak23@gmail.com>
2020-08-14 17:53:51 +0700

    Updates

---
+++ precache-manifest.ab346dec36711fdf6b11aee54a4e9c62.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a54208bb960d49def87909ccc233d2",
+    "url": "/bloggerku/index.html"
+  },
+  {
+    "revision": "b6b208c6c53ea11f7257",
+    "url": "/bloggerku/static/css/main.28d405fc.chunk.css"
+  },
+  {
+    "revision": "10a3b6037ad4637229bb",
+    "url": "/bloggerku/static/js/2.8511f5ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bloggerku/static/js/2.8511f5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6b208c6c53ea11f7257",
+    "url": "/bloggerku/static/js/main.2e7e6ab8.chunk.js"
+  },
+  {
+    "revision": "4cece7127ac7c90f1f0c",
+    "url": "/bloggerku/static/js/runtime-main.0f92ad30.js"
+  },
+  {
+    "revision": "8ec80b688921805ed2cd04b515620fba",
+    "url": "/bloggerku/static/media/1.8ec80b68.jpg"
+  },
+  {
+    "revision": "f3d801b48d9b5d69a37d4577601fa5e6",
+    "url": "/bloggerku/static/media/2.f3d801b4.jpg"
+  },
+  {
+    "revision": "8e5ed114113ca7dfb1b6ff01b7bdefe1",
+    "url": "/bloggerku/static/media/3.8e5ed114.jpg"
+  },
+  {
+    "revision": "4093bd26c161d4080a3489dc28fe2cc8",
+    "url": "/bloggerku/static/media/4.4093bd26.jpg"
+  },
+  {
+    "revision": "c59a793cc0a36479ce42f3f981c332c2",
+    "url": "/bloggerku/static/media/profile1.c59a793c.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.197b212a6c45e85c86a702b01cf1315b.js"
+  "/bloggerku/precache-manifest.ab346dec36711fdf6b11aee54a4e9c62.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bloggerku/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
