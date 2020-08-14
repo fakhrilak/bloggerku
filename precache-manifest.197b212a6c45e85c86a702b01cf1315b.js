dbe5acc472de52b66ac5874f584a932dbc809a37
fakhrilak <fakhrilak23@gmail.com>
2020-08-14 17:36:53 +0700

    Updates

---
+++ precache-manifest.197b212a6c45e85c86a702b01cf1315b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae7439b7b671b00dd166fab40137157",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1e4cc9d9e282ea1b6d8",
+    "url": "/static/css/main.28d405fc.chunk.css"
+  },
+  {
+    "revision": "10a3b6037ad4637229bb",
+    "url": "/static/js/2.8511f5ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8511f5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e4cc9d9e282ea1b6d8",
+    "url": "/static/js/main.e6d074d1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8ec80b688921805ed2cd04b515620fba",
+    "url": "/static/media/1.8ec80b68.jpg"
+  },
+  {
+    "revision": "f3d801b48d9b5d69a37d4577601fa5e6",
+    "url": "/static/media/2.f3d801b4.jpg"
+  },
+  {
+    "revision": "8e5ed114113ca7dfb1b6ff01b7bdefe1",
+    "url": "/static/media/3.8e5ed114.jpg"
+  },
+  {
+    "revision": "4093bd26c161d4080a3489dc28fe2cc8",
+    "url": "/static/media/4.4093bd26.jpg"
+  },
+  {
+    "revision": "c59a793cc0a36479ce42f3f981c332c2",
+    "url": "/static/media/profile1.c59a793c.jpeg"
+  }
+]);
