0484bcd87fb4460ab24da5c0080d5ca32f8be339
fakhrilak <fakhrilak23@gmail.com>
2020-10-13 13:26:14 +0700

    Updates

--- precache-manifest.bd46b8b04661456e25408a3c9c73dd1c.js
+++ precache-manifest.bd46b8b04661456e25408a3c9c73dd1c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "248fa98fb275bd3d7b56cee6c2e52675",
+    "revision": "2dad1758770a0091e9e9d1aed8603029",
     "url": "/bloggerku/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/bloggerku/static/css/2.8fbedf3e.chunk.css"
   },
   {
-    "revision": "aaa792af3a4318a596ac",
+    "revision": "9280e4bc5ab600324922",
     "url": "/bloggerku/static/css/main.2f0d7670.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/bloggerku/static/js/2.677defd6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aaa792af3a4318a596ac",
-    "url": "/bloggerku/static/js/main.b34a959d.chunk.js"
+    "revision": "9280e4bc5ab600324922",
+    "url": "/bloggerku/static/js/main.68066782.chunk.js"
   },
   {
     "revision": "4cece7127ac7c90f1f0c",
